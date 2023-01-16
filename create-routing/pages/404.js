994237075ed79b19c72e67c2814344c6f5bcfa18
Shubh0412 <shubh42212@gmail.com>
2023-01-16 20:32:40 +0530

    commit

---
+++ create-routing/pages/404.js
@@ -0,0 +1,5 @@
+function PageNotFound() {
+  return <h1>Custom 404 Not Found Error Page</h1>;
+}
+
+export default PageNotFound;
--- create-routing/pages/product/index.js
+++ create-routing/pages/product/index.js
@@ -1,9 +1,31 @@
-function Product() {
+import Link from "next/link";
+
+function Product({ productId = 100 }) {
   return (
     <>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href="/">
+        <p>Home</p>
+      </Link>
+      <h1>
+        <Link href="/product/1">
+          <p>Product 1</p>
+        </Link>
+      </h1>
+      <h1>
+        <Link href="/product/2">
+          <p>Product 2</p>
+        </Link>
+      </h1>
+      <h1>
+        <Link href="/product/3" replace>
+          <p>Product 3</p>
+        </Link>
+      </h1>
+      <h1>
+        <Link href={`/product/${productId}`}>
+          <p>Product {productId}</p>
+        </Link>
+      </h1>
     </>
   );
 }
